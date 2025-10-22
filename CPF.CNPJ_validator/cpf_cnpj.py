e183f91a004dad38839ab35f888a197bfe1b4614
frosty <161754446+6Sixfrosty@users.noreply.github.com>
2025-10-22 15:01:26 -0300

    Update cpf_cnpj.py

--- CPF.CNPJ_validator/cpf_cnpj.py
+++ CPF.CNPJ_validator/cpf_cnpj.py
@@ -11,11 +11,11 @@ input_number = input("Informe seu CPF ou CNPJ:\n")
 
 number_only = unmask(input_number)
 
-if len(number_only) == 11 and cpf.validate(input_number):
-    formated = cpf.mask(input_number)
-    print(f"CPF: {formated} é valido")
-elif len(number_only) == 14 and cnpj.validate(input_number):
-    formated = cnpj.mask(input_number)
+if len(number_only) == 11 and cpf.validate(number_only):
+        formated = cpf.mask(number_only)
+        print(f"CPF: {formated} é valido")
+elif len(number_only) == 14 and cnpj.validate(number_only):
+    formated = cnpj.mask(number_only)
     print(f"CNPJ: {formated} é valido")
 else:
     print("Seu CPF ou CNPJ não existe ou é inválido")
